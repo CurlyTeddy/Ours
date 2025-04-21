244936fbe99bdffed1016d7b30f69afae551d5b6
Kang Hao Hsieh <tedkhhsieh@gmail.com>
2025-04-20 23:35:58 -0700

    feat: add create account link

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import {
-  AtSymbolIcon,
+  UserIcon,
   KeyIcon,
   ExclamationCircleIcon,
   ArrowRightIcon,
@@ -10,6 +10,7 @@ import { Button } from './button';
 import { useActionState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { authenticate } from '../lib/actions';
+import Link from 'next/link';
 
 export default function LoginForm() {
   const callbackUrl = useSearchParams().get('callbackUrl') || '/';
@@ -21,24 +22,25 @@ export default function LoginForm() {
         <h1 className="mb-3 text-2xl">
           Please log in to continue.
         </h1>
-        <div className="w-full">
+        <div className="w-full space-y-5">
           <div>
             <label
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
+              htmlFor="username"
             >
-              Email
+              Username
             </label>
             <div className="relative">
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
+                id="username"
+                type="text"
+                name="username"
+                placeholder="Enter your username"
+                autoComplete="username"
                 required
               />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <UserIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
           <div className="mt-4">
@@ -55,23 +57,28 @@ export default function LoginForm() {
                 type="password"
                 name="password"
                 placeholder="Enter password"
+                autoComplete="current-password"
                 required
                 minLength={8}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          <input type="hidden" name="redirectTo" value={callbackUrl} />
+          <Button className="mt-4 w-full" aria-disabled={isPending}>
+            Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          </Button>
+          <div className="mt-6 text-sm text-gray-500 opacity-80">
+            <span className="text-gray-600">Have an invite code?</span>{' '}
+            <Link href={'/signup'} className="text-blue-600 underline hover:text-blue-800 transition-colors">Create an account</Link>
+          </div>
         </div>
-        <input type="hidden" name="redirectTo" value={callbackUrl} />
-        <Button className="mt-4 w-full" aria-disabled={isPending}>
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
         <div className="flex h-8 items-end space-x-1" aria-live="polite" aria-atomic="true">
           {errorMessage &&
-            <>
+            <div className="flex items-center space-x-1">
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
               <p className="text-sm text-red-500">{errorMessage}</p>
-            </>
+            </div>
           }
         </div>
       </div>
