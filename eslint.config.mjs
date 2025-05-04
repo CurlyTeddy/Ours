eb5b3b7e0e6986d39afe4085701d39ba1f024e4a
Kang Hao Hsieh <tedkhhsieh@gmail.com>
2025-05-03 17:48:27 -0700

    chore: add more linter check

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:@typescript-eslint/recommended",
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+  ),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      'quotes': ['error', 'double', { avoidEscape: true, allowTemplateLiterals: true }],
+      'semi': ['error', 'always'],
+    },
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      }
+    },
+  },
 ];
 
 export default eslintConfig;
