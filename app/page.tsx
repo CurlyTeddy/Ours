eccea4a33ab5cdaf429c0ede9ca9e391271a3386
Kang Hao Hsieh <tedkhhsieh@gmail.com>
2025-05-11 22:51:32 -0700

    style: wrap login link into shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="relative min-h-screen">
-      <Link
-        href="/login"
-        className="absolute bottom-32 left-1/2 -translate-x-1/2 rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-      >
-        <span>Log in</span>
-      </Link>
+      <Button asChild size={"lg"}>
+        <Link
+          href="/login"
+          className="absolute bottom-32 left-1/2 -translate-x-1/2 rounded-lg px-6 py-3 font-medium transition-colors md:text-base"
+        >
+          <span>Log in</span>
+        </Link>
+      </Button>
     </div>
   );
 }
